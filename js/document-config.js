c07ed041af638cefe005c251720a809584a41b92
Geonovum <>
2024-09-30 12:49:04 +0000

    Update from https://github.com/Geonovum/word2werkversie/commit/dd636fe54a72fed758e70ecf2b66221d04946ef2

---
+++ js/document-config.js
@@ -0,0 +1,39 @@
+var documentConfig =
+{
+  title: "Handreiking Visualisatie (PMKL)",
+  subtitle: "versie 3.0rc-intern",
+  shortName: "PMKL30rc-intern",
+  pubDomain: "kl",
+  specStatus: "wv",
+  specType: "st",
+  license: "cc-by-nd",
+  latestVersion: [
+    "https://docs.geostandaarden.nl/kl/PMKL30rc-intern/"
+  ],
+  publishDate: "2024-09-30",
+  publishVersion: "3.0.0",
+  thisVersion: [
+    "https://geonovum.github.io/kl-PMKL30rc-intern/"
+  ],
+  edDraftURI: "https://geonovum.github.io/kl-PMKL30rc-intern/",
+  canonicalURI: "https://docs.geostandaarden.nl/kl/PMKL30rc-intern/",
+  authors: [
+    {
+      name: "Edward Mac Gillavry",
+      company: "Webmapper",
+      companyURL: "https://webmapper.net/"
+    }
+  ],
+  editors: [
+    {
+      name: "Paul Janssen",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    },
+    {
+      name: "Edward Mac Gillavry",
+      company: "Webmapper",
+      companyURL: "https://webmapper.net/"
+    }
+  ]
+}
